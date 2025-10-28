66b28a90c9587b48ed29facee39740992bac20a3
Abhinav Devaansh <abhinavdevaansh@Abhinavs-MacBook-Air.local>
2025-10-29 03:24:24 +0530

    updated config

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get("/api", (req, res) => {
   res.json({ message: "This is an API response!" });
 });
 
+if(process.env.NODE_ENV !== 'production'){
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+}
+
+// ✅ Export the app for Vercel
+module.exports = app;
